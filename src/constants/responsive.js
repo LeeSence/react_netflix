de5dc9a38c5392b7e4e6953ff01cf552f1ab4eb6
JongHo Lee <jongho89822@gmail.com>
2024-11-04 20:52:08 +0900

    mobile_screen_update

--- src/constants/responsive.js
+++ src/constants/responsive.js
@@ -1,17 +1,37 @@
 export const responsive = {
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
+  largeDesktop: {
+    breakpoint: { max: 3000, min: 1440 },
     items: 6,
-    slidesToSlide: 3, // optional, default to 1.
+    slidesToSlide: 3,
+  },
+  desktop: {
+    breakpoint: { max: 1440, min: 1024 },
+    items: 4,
+    slidesToSlide: 2,
+  },
+  smallDesktop: {
+    breakpoint: { max: 1024, min: 768 },
+    items: 3,
+    slidesToSlide: 2,
+  },
+  largeTablet: {
+    breakpoint: { max: 768, min: 600 },
+    items: 3,
+    slidesToSlide: 1,
   },
   tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2,
-    slidesToSlide: 2, // optional, default to 1.
+    breakpoint: { max: 600, min: 480 },
+    items: 3,
+    slidesToSlide: 1,
+  },
+  largeMobile: {
+    breakpoint: { max: 480, min: 360 },
+    items: 3,
+    slidesToSlide: 1,
   },
   mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
-    slidesToSlide: 1, // optional, default to 1.
+    breakpoint: { max: 360, min: 0 },
+    items: 3,
+    slidesToSlide: 1,
   },
 };
